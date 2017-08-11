c013b9ba9782565127a88c5bc5071f473029a804
lgdcnm <18268815524@163.com>
2017-08-11 11:34:22 +0800

    t

--- js/index.js
+++ js/index.js
@@ -22,4 +22,30 @@ $(function(){
 			$("nav ul").slideToggle();
 		});
 	}
+	
+	
+	
+//	slide
+	$("slide .tele").mouseover(function(){
+		$("slide .popup").removeClass("active");
+		$(this).next().addClass("active");
+	});
+	
+	
+//	nav点击效果
+	$(".twonav div").click(function(){
+		var index = $(this).index()+1;
+		
+		if(index >= $(".twonav div").length){
+			index = 0;
+		}
+		var top = $(".navclick:eq("+index+")").offset().top-268;
+		console.log(index);
+//		$(window).scrollTop.animate(function(){
+//			$(window).scrollTop(top);
+//		},1000);
+		$('html,body').animate({
+			scrollTop:top
+		},1000);
+	});
 })
